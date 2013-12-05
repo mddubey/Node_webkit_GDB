98e301cf8690391d58df0c7f038c9d9b8930c738
MD Dubey <dubeyg@whatever.com>
2013-12-05 12:12:05 +0530

    Output in a seprated div

--- GDB_Ui/startDebug.js
+++ GDB_Ui/startDebug.js
@@ -1,19 +1,24 @@
-function debug () {
-    spawn = require('child_process').spawn;
+function openDebugWindow(){
     var fileName = $('#exeFile')[0].value;
     fileName = fileName.replace(/\\/g,'/');
     console.log(fileName);
-	var ls    = spawn('gdb', [fileName]); // the second arg is the command 
-                                          // options
+	var nextPageContent = require('fs').readFileSync('commands.html','utf-8');
+	document.write(nextPageContent);
+	startDebug(fileName);
+}
 
-	ls.stdout.on('data', function (data) {    // register one or more handlers
-	  document.write(data);
+function startDebug (fileName) {
+    spawn = require('child_process').spawn;
+	var gdb = spawn('gdb', [fileName]); // the second arg is the command 
+	                                          // options
+	gdb.stdout.on('data', function (data) {    // register one or more handlers
+	  	$('#output')[0].textContent = $('#output')[0].textContent.concat(data);
 	});
 
-	ls.stderr.on('data', function (data) {
+	gdb.stderr.on('data', function (data) {
 	  console.log('stderr: ' + data);
 	});
 
-	ls.on('exit', function (code) {
+	gdb.on('exit', function (code) {
 	});
 }

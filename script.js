c6d9cc69a5e699bc781c3a5241d07cb2098a6c46
MD Dubey <mdinfotech007@gmail.com>
2015-01-10 19:58:03 +0530

    Changed text area to table

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ var interface = {};
 
 interface.onFullCode = function(fullCodeContent) {
 	var codeLines = fullCodeContent.split('\n');
+	var tbody = '';
 	codeLines = codeLines.map(function(line) {
 		var linePartes = line.split('\t');
 		var lineNumber = linePartes[0];
-		var codeInLine = linePartes.slice(1).join('\t');
+		linePartes[0] = '';
+		var codeInLine = linePartes.join('&nbsp;&nbsp;&nbsp;');
 		return {
 			lineNumber: lineNumber,
 			codeInLine: codeInLine
 		};
 	});
-	jQuery('#codeWindow').text(fullCodeContent);
+	codeLines.forEach(function(line, index) {
+		var trHtml = '<tr><td data-line="VALUE">LINE</td><td>CODE</td></tr>';
+		trHtml = trHtml.replace('LINE', index + 1).replace('VALUE', line.lineNumber).replace('CODE', line.codeInLine);
+		tbody += trHtml;
+	});
+	// console.log(tbody)
+	jQuery('#codeWindow').html(tbody);
 }
